09a9addfd8dde87b773ba995a67bc253a604b9d7
bryansmith85 <bryansmith1@ufl.edu>
2023-02-28 18:51:41 -0500

    Added tic-tac-toe service

--- src/gator-arcade/src/app/tic-tac-toe/tic-tac-toe.service.ts
+++ src/gator-arcade/src/app/tic-tac-toe/tic-tac-toe.service.ts
@@ -1,86 +1,104 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
+interface Board {
+  board: string[][];
+}
 
+interface Player {
+  name: string;
+  marker: string;
+}
 
-  interface NewGame {
-    player1: string;
-    player2: string;
+interface Game {
+  b: Board;
+  p: Player;
+}
+
+interface Move {
+  row: number;
+  col: number;
+}
+
+interface NewGame {
+  player1: string;
+  player2: string;
+}
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicTacToeService {
+
+  private apiUrl = 'http://localhost:3000/games';
+
+  constructor(private http: HttpClient) { }
+
+  //Sends an HTTP POST request to create a new game on the server.
+  public createGame(game: NewGame): Observable<Game> {
+    return this.http.post<Game>(this.apiUrl, game, httpOptions).pipe(
+      catchError(this.handleError<Game>('createGame'))
+    );
   }
 
-  export interface Game {
-    id: string;
-    player1: string;
-    player2: string;
-    currentPlayer: string;
-    winner: string;
-    b:  {
-      board: string[][];
-    }
+  //Sends an HTTP GET request to retrieve a game from the server.
+  public getGame(id?: string): Observable<Game[] | Game> {
+    const url = id ? `${this.apiUrl}/${id}` : this.apiUrl;
+    return this.http.get<Game[] | Game>(url).pipe(
+      catchError(this.handleError<Game[] | Game>('getGame'))
+    );
   }
 
-  const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-
-  const DEFAULT_PLAYER = 'X';
-
-  @Injectable({
-    providedIn: 'root'
-  })
-
-  export class TicTacToeService {
-    private apiUrl = 'http://localhost:8080/api';  // URL to web api
-  
-
-    constructor(private http: HttpClient) { }
-
-     // Creates a new game by sending a POST request to the API with the specified game object.
-    public createGame(game: NewGame): Observable<Game> {
-      return this.http.post<Game>(this.apiUrl, game, httpOptions).pipe(
-        catchError(this.handleError<Game>('createGame'))
-      );
-    }
-  
-      // Retrieves a game from the API by sending a GET request to the specified URL. If an ID is provided, only that game is retrieved.
-    public getGame(id?: string): Observable<Game[] | Game> {
-      const url = id ? `${this.apiUrl}/${id}` : this.apiUrl;
-      return this.http.get<Game[] | Game>(url).pipe(
-        catchError(this.handleError<Game[] | Game>('getGame'))
-      );
-    }
-  
-      // Updates a game by sending a PUT request to the API with the specified game object and ID.
-    public updateGame(game: Game): Observable<Game> {
-      const url = `${this.apiUrl}/${game.id}`;
-      return this.http.put<Game>(url, game, httpOptions).pipe(
-        catchError(this.handleError<Game>('updateGame'))
-      );
-    }
-  
-      // Deletes a game from the API by sending a DELETE request to the specified URL.
-    public deleteGame(id: string): Observable<Game> {
-      const url = `${this.apiUrl}/${id}`;
-      return this.http.delete<Game>(url, httpOptions).pipe(
-        catchError(this.handleError<Game>('deleteGame'))
-      );
-    }
-
-    // Makes a move in the specified game by sending a POST request to the API with the specified move object and game ID.
-    public makeMove(id: string, move: { row: number, col: number }): Observable<Game> {
-      const url = `${this.apiUrl}/${id}/play-move`;
-      return this.http.post<Game>(url, move);
-    }
-    
-    // Handles any errors that occur during API requests by logging the error and returning a default value.
-    private handleError<T>(operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
-        console.error(error);
-        return of(result as T);
-      };
-    }
+  //Sends an HTTP PUT request to update a game on the server.
+  public updateGame(game: Game): Observable<Game> {
+    const url = `${this.apiUrl}/${game.b.board[0][0]}`;
+    return this.http.put<Game>(url, game, httpOptions).pipe(
+      catchError(this.handleError<Game>('updateGame'))
+    );
   }
+
+  //Sends an HTTP DELETE request to delete a game from the server.
+  public deleteGame(id: string): Observable<Game> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<Game>(url, httpOptions).pipe(
+      catchError(this.handleError<Game>('deleteGame'))
+    );
+  }
+
+  //Sends an HTTP PUT request to update a player on the server and to update the game with the new board.
+  public makeMove(id: string, move: Move): Observable<Game> {
+    const url = `${this.apiUrl}/${id}/play-move`;
+    const player = id === '1' ? 'player1' : 'player2';
+    const game = {
+      b: {
+        board: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', '']
+        ]
+      },
+      p: {
+        name: player,
+        marker: player === 'player1' ? 'X' : 'O'
+      }
+    };
+    return this.getGame(id).pipe(
+      map((games: Game[] | Game) => Array.isArray(games) ? games[0] : games),
+      catchError(this.handleError<Game>('makeMove'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+}
