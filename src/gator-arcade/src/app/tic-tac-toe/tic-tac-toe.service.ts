3b6319f46d61c8768b84a80cf8d16f3efe42352b
bryansmith85 <bryansmith1@ufl.edu>
2023-03-01 17:54:22 -0500

    Update Service Testing

--- src/gator-arcade/src/app/tic-tac-toe/tic-tac-toe.service.ts
+++ src/gator-arcade/src/app/tic-tac-toe/tic-tac-toe.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
@@ -13,17 +13,17 @@ interface Player {
   marker: string;
 }
 
-interface Game {
+export interface Game {
   b: Board;
   p: Player;
 }
 
-interface Move {
+export interface Move {
   row: number;
   col: number;
 }
 
-interface NewGame {
+export interface NewGame {
   player1: string;
   player2: string;
 }
@@ -37,7 +37,10 @@ const httpOptions = {
 })
 export class TicTacToeService {
 
-  private apiUrl = 'http://localhost:3000/games';
+  private game: Game = { b: { board: [['', '', ''], ['', '', ''], ['', '', '']] }, p: { name: '', marker: '' } };
+
+
+  public apiUrl = 'http://localhost:3000/games';
 
   constructor(private http: HttpClient) { }
 
@@ -64,18 +67,142 @@ export class TicTacToeService {
     );
   }
 
-  //Sends an HTTP DELETE request to delete a game from the server.
-  public deleteGame(id: string): Observable<Game> {
-    const url = `${this.apiUrl}/${id}`;
-    return this.http.delete<Game>(url, httpOptions).pipe(
-      catchError(this.handleError<Game>('deleteGame'))
-    );
+  //Updates the game board with a valid move and returns the updated game, or returns the original game if the move is invalid.
+  public updateBoard(game: Game, move: Move): Game {
+    const { row, col } = move;
+    if (row > 2 || row < 0 || col > 2 || col < 0) {
+      console.log("Error: Out of bounds");
+      return game;
+    } else if (game.b.board[row][col] !== "") {
+      console.log("Spot already taken. Please choose another spot.");
+      return game;
+    } else {
+      game.b.board[row][col] = game.p.marker;
+      return game;
+    }
+  }
+
+  //Logs the current state of the game board, replacing any empty spots with dashes.
+  public displayBoard(): void {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.game.b.board[i][j] == "") {
+          this.game.b.board[i][j] = "-";
+        }
+        console.log(this.game.b.board[i][j]);
+      }
+      console.log();
+    }
+  }
+
+  //Checks whether the current player has won the game by checking all possible winning combinations in the game board.
+  public checkIfWon(): boolean {
+    /* There are only 8 possible winning combinations of tic tac toe:
+       3 consecutive horizontal, vertical, or diagonal.
+       Check if any of these are present for the current player.
+    */
+    let playerWon = false;
+    let row = 0;
+    let count = 0;
+  
+    // Checking all rows for winning combinations
+    while (row < 3 && !playerWon) {
+      let currentRow = this.game.b.board[row];
+  
+      for (let i = 0; i < currentRow.length; i++) {
+        if (currentRow[i] !== this.game.p.marker) {
+          break;
+        } else {
+          count++;
+          if (count === 3) {
+            playerWon = true;
+            return playerWon;
+          }
+        }
+      }
+      row++;
+      count = 0;
+    }
+  
+    let col = 0;
+    count = 0;
+    let currentCol: string[] = [];
+  
+    // Checking all columns for winning combinations
+    while (col < 3 && !playerWon) {
+      currentCol = [this.game.b.board[0][col], this.game.b.board[1][col], this.game.b.board[2][col]];
+  
+      for (let i = 0; i < currentCol.length; i++) {
+        if (currentCol[i] !== this.game.p.marker) {
+          playerWon = false;
+          break;
+        } else {
+          count++;
+          if (count === 3) {
+            playerWon = true;
+            return playerWon;
+          }
+        }
+      }
+      col++;
+      count = 0;
+    }
+  
+    // Checking diagonals for winning combinations
+    let diagonalOne = [this.game.b.board[0][0], this.game.b.board[1][1], this.game.b.board[2][2]];
+    let diagonalTwo = [this.game.b.board[0][2], this.game.b.board[1][1], this.game.b.board[2][0]];
+    count = 0;
+  
+    for (let i = 0; i < diagonalOne.length; i++) {
+      if (diagonalOne[i] !== this.game.p.marker) {
+        playerWon = false;
+        break;
+      } else {
+        count++;
+        if (count === 3) {
+          playerWon = true;
+          return playerWon;
+        }
+      }
+    }
+  
+    count = 0;
+    for (let i = 0; i < diagonalTwo.length; i++) {
+      if (diagonalTwo[i] !== this.game.p.marker) {
+        playerWon = false;
+        break;
+      } else {
+        count++;
+        if (count === 3) {
+          playerWon = true;
+          break;
+        }
+      }
+    }
+    return playerWon;
+  }
+
+  //Checks if a game has ended in a draw by iterating over its entire board and returning true if all positions are occupied.
+  public isDraw(game: Game): boolean {
+    let gameIsDraw = true;
+  
+    // Check over entire board to see if it is full
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (game.b.board[i][j] == "") {
+          gameIsDraw = false;
+        }
+      }
+    }
+  
+    return gameIsDraw;
   }
 
   //Sends an HTTP PUT request to update a player on the server and to update the game with the new board.
   public makeMove(id: string, move: Move): Observable<Game> {
     const url = `${this.apiUrl}/${id}/play-move`;
-    const player = id === '1' ? 'player1' : 'player2';
+    const player = id === '123' ? 'player1' : 'player2';
+    const marker = player === 'player1' ? 'X' : 'O';
     const game = {
       b: {
         board: [
@@ -86,16 +213,23 @@ export class TicTacToeService {
       },
       p: {
         name: player,
-        marker: player === 'player1' ? 'X' : 'O'
+        marker
       }
     };
-    return this.getGame(id).pipe(
-      map((games: Game[] | Game) => Array.isArray(games) ? games[0] : games),
+    const body = {
+      row: move.row,
+      col: move.col,
+      player,
+      marker
+    };
+    return this.http.put<Game>(url, body).pipe(
+      tap((game: Game) => this.updateBoard(game, move)),
       catchError(this.handleError<Game>('makeMove'))
     );
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
+  //Handles errors from HTTP requests and returns an Observable with a default result.
+  public handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       return of(result as T);
--- src/gator-arcade/src/app/tic-tac-toe/tic-tac.toe.service.spec.ts
+++ src/gator-arcade/src/app/tic-tac-toe/tic-tac.toe.service.spec.ts
@@ -21,6 +21,7 @@ describe('TicTacToeService', () => {
     httpMock.verify();
   });
 
+  //Verifies that the createGame method sends a POST request to the server to create a new game and returns it.
   describe('createGame', () => {
     it('should create a new game and return it', () => {
       const newGame: NewGame = { player1: 'player1', player2: 'player2' };
@@ -48,6 +49,7 @@ describe('TicTacToeService', () => {
     });
   });
 
+  //Tests the 'getGame' function of the service by checking if it retrieves a specific game can be retrieved by its ID.
   describe('getGame', () => {
     it('should retrieve a specific game', () => {
       const id = '12345';
@@ -114,6 +116,7 @@ describe('TicTacToeService', () => {
     });
   });
 
+  //Checks if updateGame sends an HTTP PUT request to update a game on the server, and handles errors when updating a game.
   describe('updateGame', () => {
     it('should send an HTTP PUT request to update a game on the server', () => {
       const game = {
@@ -184,6 +187,133 @@ describe('TicTacToeService', () => {
     });
   });
 
+  //Checks if updateBoard sends an HTTP PUT request to update a game on the server, and handles errors when updating a game.
+  describe('updateBoard', () => {
+    it('should update the game board with a valid move', () => {
+      // Arrange
+      const game: Game = {
+        b: {
+          board: [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+          ]
+        },
+        p: {
+          name: 'player1',
+          marker: 'X'
+        }
+      };
+      const move: Move = {
+        row: 0,
+        col: 0
+      };
+  
+      // Act
+      const updatedGame = service.updateBoard(game, move);
+  
+      // Assert
+      expect(updatedGame.b.board[0][0]).toEqual('X');
+    });
+  
+    it('should not update the game board with an invalid move', () => {
+      // Arrange
+      const game: Game = {
+        b: {
+          board: [
+            ['X', 'O', ''],
+            ['', 'O', ''],
+            ['', '', 'X']
+          ]
+        },
+        p: {
+          name: 'player2',
+          marker: 'O'
+        }
+      };
+      const move: Move = {
+        row: 0,
+        col: 0
+      };
+  
+      // Act
+      const updatedGame = service.updateBoard(game, move);
+  
+      // Assert
+      expect(updatedGame).toEqual(game);
+    });
+  });
+
+  //Checks if the displayBoard method sends an HTTP GET request to display the board with dashes.
+  describe('displayBoard', () => {
+    it('should display the game board with dashes', () => {
+      // Arrange
+      const service = TestBed.get(TicTacToeService);
+      const expectedOutput = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+      
+      // Act
+      service.displayBoard();
+    
+      // Assert
+      expect(service.game.b.board).toEqual(expectedOutput);
+    });
+  });
+
+  //Checks the checkifWon method and returns true if a player has won, and false if a player has not won.
+  describe('checkIfWon', () => {
+    it('should return true if a player has won', () => {
+      const game = { b: { board: [['X', '', 'O'], ['X', 'O', ''], ['X', '', '']] }, p: { name: 'player1', marker: 'X' } };
+      service['game'] = game; // Set the game object to the winning board state
+      expect(service.checkIfWon()).toBeTruthy(); // Expect that a player has won
+    });
+  
+    it('should return false if a player has not won', () => {
+      const game = { b: { board: [['X', '', 'O'], ['', 'O', ''], ['', '', '']] }, p: { name: 'player1', marker: 'X' } };
+      service['game'] = game; // Set the game object to a non-winning board state
+      expect(service.checkIfWon()).toBeFalsy(); // Expect that a player has not won
+    });
+  });
+
+  //Checks the isDraw method and returns true for a draw game board and false for a non-draw game board.
+  describe('isDraw', () => {
+    it('should return true for a draw game', () => {
+      const game: Game = {
+        b: {
+          board: [
+            ['X', 'O', 'X'],
+            ['X', 'O', 'O'],
+            ['O', 'X', 'X']
+          ]
+        },
+        p: {
+          name: 'player1',
+          marker: 'X'
+        }
+      };
+  
+      expect(service.isDraw(game)).toBe(true);
+    });
+  
+    it('should return false for a non-draw game', () => {
+      const game: Game = {
+        b: {
+          board: [
+            ['X', 'O', ''],
+            ['X', 'O', 'O'],
+            ['O', 'X', 'X']
+          ]
+        },
+        p: {
+          name: 'player1',
+          marker: 'X'
+        }
+      };
+  
+      expect(service.isDraw(game)).toBe(false);
+    });
+  });
+
+  //Checks the makeMove method by checking it it returns an Observable of Game object and sends a PUT request to update the game board with the move made by the player.
 describe('makeMove', () => {
   it('should return an Observable<Game>', () => {
     const mockId = '123';
@@ -210,8 +340,30 @@ describe('makeMove', () => {
     });
 
     const req = httpMock.expectOne(url);
-    expect(req.request.method).toBe('GET');
+    expect(req.request.method).toBe('PUT');
     req.flush(mockResponse);
+  });
+});
+
+//Logs the given error message to the console, and returns an observable of the given result.
+describe('handleError', () => {
+  it('should log the error and return an Observable of the given result', () => {
+    const errorMessage = 'test error';
+    const result = { data: 'test data' };
+    const operation = 'test operation';
+    const handleError = service.handleError(operation, result);
+
+    const consoleSpy = spyOn(console, 'error');
+    const nextSpy = jasmine.createSpy('next');
+    const errorSpy = jasmine.createSpy('error');
+    const completeSpy = jasmine.createSpy('complete');
+
+    handleError(errorMessage).subscribe(nextSpy, errorSpy, completeSpy);
+
+    expect(consoleSpy).toHaveBeenCalledWith(errorMessage);
+    expect(nextSpy).toHaveBeenCalledWith(result);
+    expect(errorSpy).not.toHaveBeenCalled();
+    expect(completeSpy).toHaveBeenCalled();
   });
 });
 });
