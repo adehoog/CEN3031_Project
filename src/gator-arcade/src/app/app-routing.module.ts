4dad40fc929d9a0ebe25d707efc49939719483f5
Bryan Smith <bryansmith1@ufl.edu>
2023-03-27 17:48:27 -0400

    Fixed Tic Tac Toe Redirect

--- src/gator-arcade/src/app/app-routing.module.ts
+++ src/gator-arcade/src/app/app-routing.module.ts
@@ -5,15 +5,15 @@ import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 import { ConnectFourComponent } from './connect-four/connect-four.component';
 import { SolitaireComponent } from './solitaire/solitaire.component';
 import { WarComponent } from './war/war.component';
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  //{ path: '', component: AppComponent },
   { path: 'tic-tac-toe', component: TicTacToeComponent },
   { path: 'checkers', component: CheckersComponent },
   { path: 'connect-four', component: ConnectFourComponent },
   { path: 'solitaire', component: SolitaireComponent },
   { path: 'war', component: WarComponent },
-  { path: '', redirectTo: '/tic-tac-toe', pathMatch: 'full' }
 ];
 
 
