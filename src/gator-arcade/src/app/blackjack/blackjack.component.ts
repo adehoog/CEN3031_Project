dbb3c57fdb947b5280128f28e626370948836e4d
adehoog <adehoog@ufl.edu>
2023-03-28 21:22:30 -0400

    Blackjack visuals

--- src/gator-arcade/src/app/blackjack/blackjack.component.ts
+++ src/gator-arcade/src/app/blackjack/blackjack.component.ts
@@ -6,25 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./blackjack.component.scss']
 })
 export class BlackjackComponent {
-  totalMoney: number = 0;
-  currentBet: number = 0;
+  //player vars
+  totalMoney: number = 10;
+  currentBet: number = 1;
+  roundTotal: number = 0;
   hasWon: boolean = false;
+  //[cardRank, cardSuit]
+  cards: string[][] = [[]];
+
+  //dealer vars
+  dealerRoundTotal: number = 0;
+  dealerBet: number = 1;
+
+  //game vars
+  bettingPool: number = 1;
 
   increaseBet() {
     if(this.currentBet < this.totalMoney){
       this.currentBet++;
+      this.updatePool();
+      //console.log(this.currentBet);
     }
   }
 
   decreaseBet() {
-    if(this.currentBet > 0){
+    if(this.currentBet > 1){
       this.currentBet--;
+      //console.log(this.currentBet);
     }
+    this.updatePool();
   }
 
-  checkWin() {
+  updatePool() {
+    this.bettingPool = this.dealerBet + this.currentBet;
+  }
+
+  addCardVal(cardVal: number){
+    this.roundTotal += cardVal;
+  }
+
+  addCard(cardRank: string, cardSuit: string){
+    const newCard = [cardRank, cardSuit];
+    this.cards.push(newCard);
+  }
+
+  checkWin(){
     if(this.hasWon){
-      
+      this.totalMoney += (this.bettingPool - this.currentBet);
+    }
+    else{
+      this.totalMoney -= this.currentBet;
     }
   }
 
